af9c820fe4128fb96723a96148ec8d22195c98b2
Илья Казютин <gorgutz1@yandex.ru>
2020-10-31 17:22:58 +0300

    Добавьте файлы проекта.

---
+++ Numerical_Methods_2/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Numerical_Methods_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisY.Maximum = 10D;
+            chartArea1.AxisY.Minimum = -10D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Series4";
+            series5.BorderWidth = 3;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Legend = "Legend1";
+            series5.Name = "Series5";
+            series6.BorderWidth = 3;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series6.Legend = "Legend1";
+            series6.Name = "Series6";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(961, 535);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(437, 553);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 588);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Numerical_Methods_2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numerical_Methods_2
+{
+    public partial class Form1 : Form
+    {
+        const double Pi = 3.14;
+        const double h = Pi / 7;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double f(double x)
+        {
+            return Math.Sin(x) * Math.Sqrt(x) + 1;
+        }
+
+        double f_const(int k)
+        {
+            double[] x_array = new double[5] {h / 2, h * 5 / 2, h * 13 / 2, h * 9 / 2, h * 3 / 2};
+            double FuncSum = 0;
+            double FuncMult = 1;
+            int i = 1, j = 1;
+            while (i <= k)
+            {
+                while (j <= k)
+                {
+                    if (i != j) FuncMult *= x_array[i - 1] - x_array[j - 1];
+                    j++;
+                }
+                FuncSum += f(x_array[i - 1]) / FuncMult;
+                i++;
+            }
+            return FuncSum;
+        }
+
+        double Ln(double x, int n)
+        { 
+            double[] x_array = new double[5] { h / 2, h * 5 / 2, h * 13 / 2, h * 9 / 2, h * 3 / 2 };
+
+            int i = 1;
+            double FuncSum = 0;
+
+            while (i <= n)
+            {
+                switch (i)
+                {
+                    case 1:
+                        FuncSum += f(x_array[0]);
+                        break;
+                    case 2:
+                        FuncSum += f_const(2) * (x - x_array[0]);
+                        break;
+                    case 3:
+                        FuncSum += f_const(3) * (x - x_array[0]) * (x - x_array[1]);
+                        break;
+                    case 4:
+                        FuncSum += f_const(4) * (x - x_array[0]) * (x - x_array[1]) * (x - x_array[2]);
+                        break;
+                    case 5:
+                        FuncSum += f_const(5) * (x - x_array[0]) * (x - x_array[1]) * (x - x_array[2]) * (x - x_array[3]);
+                        break;
+                    default:
+                        break;
+                }
+                i++;
+            }
+            return FuncSum;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[] x_array = new double[5] { h / 2, h * 3 / 2, h * 5 / 2, h * 9 / 2, h * 13 / 2 };
+            double a = 0, b = 2 * Pi, step = 0.01;
+            double x = a + step, y;
+            this.chart1.Series[0].Points.Clear();
+
+            while (x < b)
+            {
+                y = f(x);
+                this.chart1.Series[0].Points.AddXY(x, y);
+
+                for (int i = 1; i != 6; ++i)
+                {
+                    y = Ln(x, i);
+                    this.chart1.Series[i].Points.AddXY(x, y);
+                }
+
+                x += step;
+            }
+        }
+    }
+}
